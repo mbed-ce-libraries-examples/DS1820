2afa5b64549264e41f9b59a2e0dc5039969b5c36
hudakz <hudakz@localhost>
2015-03-26 20:57:59 +0000

    Sensor array sample added

--- DS1820.h
+++ DS1820.h
@@ -6,18 +6,18 @@
 class   DS1820
 {
     OneWire oneWire;
-    uint8_t present;
     uint8_t type_s;
     uint8_t data[12];
     uint8_t addr[8];
     float   toFloat(uint16_t word);
 public:
+    uint8_t present;
+
     DS1820(PinName pin);
     DS1820(char model, PinName pin);
     bool   begin(void);
     void   setResolution(uint8_t res);
     void   startConversion(void);
     float  read(void);
-
 };
 #endif /* DS1820_H_ */
